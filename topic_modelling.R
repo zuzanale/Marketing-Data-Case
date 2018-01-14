02afbbb9d0b45d74b55b47fb726bf40d1941be5b
zuzanale <lezuzan@gmail.com>
2018-01-14 20:59:30 +0100

    topic modelling + visualisation of the models

--- topic_modelling.R
+++ topic_modelling.R
@@ -2,69 +2,147 @@
 ###needs some changes and adjustments to our corpus 
 #source: https://rpubs.com/chrisbail/nlp_and_topic_models
 
+#set working directory of the project
+setwd("C:/Users/lezuz/OneDrive/Documents/VU/Marketing Data Case/scripts")
+
+###load our corpus into environmet
+load("CORPUS_GILEAD.rda")
+str(corpusFirm) #get structure and data types of the data object
+head(unique(corpusFirm$date))
+
 library(tm)
 library(stopwords)
+library(topicmodels)
+library(dplyr)
 #corpus reformating
 corpus1=corpusFirm #make a clone
 corpus1$text=iconv(corpus1$text,"latin1","ASCII",sub="")
-blog_corpus=Corpus(VectorSource(as.vector(corpus1$text)))
+gilead_corpus=tm::Corpus(VectorSource(as.vector(corpus1$text)))
 
 #pre-processing
-blog_corpus=tm_map(blog_corpus, content_transformer(removePunctuation))
-blog_corpus=tm_map(blog_corpus,  content_transformer(tolower)) 
-blog_corpus=tm_map(blog_corpus , content_transformer(stripWhitespace))
+#gilead_corpus=tm_map(gilead_corpus, content_transformer(removePunctuation))
+gilead_corpus=tm_map(gilead_corpus,  content_transformer(tolower)) 
+gilead_corpus=tm_map(gilead_corpus , content_transformer(stripWhitespace))
 
 #stop words
-stoplist=stopwords('en')
-blog_corpus=tm_map(blog_corpus , content_transformer(removeWords), stoplist)
+#stoplist=stopwords('en')
+#gilead_corpus=tm_map(gilead_corpus , content_transformer(removeWords), stoplist)
 
 #stemming
-blog_corpus=tm_map(blog_corpus , content_transformer(stemDocument), language = "english")
+#gilead_corpus=tm_map(gilead_corpus , content_transformer(stemDocument), language = "english")
 
-#document term matrix
-Blog_DTM <- DocumentTermMatrix(blog_corpus, control = list(wordLengths = c(2, Inf)))
-inspect(Blog_DTM[1:20,1:20])
-
+#document term matrix, frequency of terms that occur in collection of documents
+#rows-documents, columns-terms
+#NPL can be performed at this step by "control" argument
+gilead_DTM <- DocumentTermMatrix(gilead_corpus, control = list(stemming = TRUE, stopwords = TRUE,
+                                                      minWordLength = 2, removeNumbers = TRUE, 
+                                                      removePunctuation = TRUE))
+inspect(gilead_DTM[1:20,1:20])
+str(gilead_DTM )
 #remove sparse terms
-DTM <- removeSparseTerms(Blog_DTM , 0.990)
+gilead_DTM=removeSparseTerms(gilead_DTM , 0.990)
+
+#reduce the number of terms based on tf-idf importancy
+term_tfidf=tapply(gilead_DTM$v/slam::row_sums(gilead_DTM)[gilead_DTM$i], gilead_DTM$j, mean) *
+  log2(tm::nDocs(gilead_DTM)/slam::col_sums(gilead_DTM > 0))
+summary(term_tfidf)
+
+#median is 0.022781
+## we remove documents with tfidf smaller than median; >= to the.022781
+gileadreduced_DTM=gilead_DTM[,term_tfidf >= .022781]
+summary(slam::col_sums(gileadreduced_DTM))
 
 #inspect popular words
-findFreqTerms(Blog_DTM, 3000) #also might need to be added into stopwords
+findFreqTerms(gileadreduced_DTM, 1000)
+findFreqTerms(gilead_DTM, 3000) #this might be better to use since
+#some important terms are dropped by tf-idf approach
 
 #number of topics
-k=10
-library(topicmodels)
-#######check the parameters
-control_LDA_Gibbs <- list(alpha = 50/k, estimate.beta = T, 
-                         verbose = 0, prefix = tempfile(), 
-                         save = 0, 
-                         keep = 50, 
-                         seed = 980, 
-                         nstart = 1, best = T,
-                         delta = 0.1,
-                         iter = 2000, 
-                         burnin = 100, 
-                         thin = 2000) 
+k=9
 
-#######check the parameters
-my_first_topic_model <- LDA(Blog_DTM, k, method = "Gibbs", 
-                            control = control_LDA_Gibbs)
+#determine the number of topics by harmonic mean
+system.time(DTM_model=topicmodels::LDA(gilead_DTM,k, 
+                                           method = "Gibbs", control = list(iter=2000, seed = 0622)))
+
+####explore the estimated model
+
+#most likely topic for each document
+DTM_topics=topicmodels::topics(DTM_model, 1)
+
+## top T terms by topic - this should be a variable in function
+T=20
+DTM_terms=as.data.frame(topicmodels::terms(DTM_model,T),stringsAsFactors =F)
+#DTM_terms[1:5] #see first 5 topics
 
 #most popular terms by topic
-terms(my_first_topic_model, 10)
+#terms(DTM_model, 10)
 
-#determine the K
-#Hat tip to Achiim Edelman for this nice function.
-many_models <- lapply(seq(2, 35, by = 1), 
-                        function(x) {LDA(Blog_DTM, x, method = "Gibbs", 
-                                         control = control_LDA_Gibbs)} )
+# Creates a dataframe to store the Lesson Number and the most likely topic
+doctopics.df=as.data.frame(DTM_topics)
+doctopics.df=dplyr::transmute(doctopics.df, docID = rownames(doctopics.df), Topic = DTM_topics)
 
-#plot the loglikelihoods
-many_models.logLik <- as.data.frame(as.matrix(lapply(many_models, logLik)))
-plot(2:35, unlist(many_models.logLik ), xlab="Number of Topics", ylab="Log-Likelihood")
+# add column with rownumber of a document to the original corpus
+corpusFirm$docID=as.character(seq.int(nrow(corpusFirm)))
 
-#which document is assigment to which topic
-topic_assignments_by_docs <- topics(my_first_topic_model)
+## Adds topic number to original corpus
+doctopics.df=dplyr::inner_join(corpusFirm, doctopics.df, by = "docID")
 
+#topic labelling for visualisaiton part
+topicTerms <- tidyr::gather(DTM_terms, Topic)
+topicTerms <- cbind(topicTerms, Rank = rep(1:T))
+topTerms <- dplyr::filter(topicTerms, Rank < 4)
+topTerms <- dplyr::mutate(topTerms, Topic = stringr::word(Topic, 2))
+topTerms$Topic <- as.numeric(topTerms$Topic)
+topicLabel <- data.frame()
+for (i in 1:k){
+  z <- dplyr::filter(topTerms, Topic == i)
+  l <- as.data.frame(paste(z[1,2], z[2,2], z[3,2], sep = " " ), stringsAsFactors = FALSE)
+  topicLabel <- rbind(topicLabel, l)
+  
+}
+colnames(topicLabel)=c("Label")
+topicLabel # this label is based on 3 top terms for each topic
+
+##extract theta-posterior probabilities of the topic per doc
+theta=as.data.frame(topicmodels::posterior(DTM_model)$topics)
+#head(theta[1:5])
+
+##################THIS IS EXTRA CODE #####################
+#### using metadata to correlate categories with thetas
+
+#using predefined doc category to correlate with a topic
+#we would have to figure out how to do grouping for document by 
+#category
+x=as.data.frame(row.names(theta), stringsAsFactors = FALSE)
+colnames(x)=c("docID")
+#x$docID=as.numeric(x$docID)
+theta2= cbind(x, theta)
+theta2=dplyr::left_join(theta2, Category, by = "docID") #Category
+#should be a data set with exlusive category for each document id
+
+## Returns column means grouped by category
+theta.mean.by <- by(theta2[, 2:k], theta2$Category, colMeans)
+theta.mean <- do.call("rbind", theta.mean.by)
+
+#corelate topics
+library(corrplot)
+c <- cor(theta.mean)
+corrplot(c, method = "circle")
+
+#most diagnostic topic for each categorie
+theta.mean.ratios=theta.mean
+for (ii in 1:nrow(theta.mean)) {
+  for (jj in 1:ncol(theta.mean)) {
+    theta.mean.ratios[ii,jj]=
+      theta.mean[ii,jj] / sum(theta.mean[ii,-jj])
+  }
+}
+topics.by.ratio=apply(theta.mean.ratios, 1, function(x) sort(x, decreasing = TRUE, index.return = TRUE)$ix)
+
+# The most diagnostic topics per category are found in the theta 1st row of the index matrix:
+topics.most.diagnostic=topics.by.ratio[1,]
+head(topics.most.diagnostic)
+
+##############################################################
 #save the whole environment for later work
-save.image(file='myEnvironment.RData')
+save.image(file='Environment.RData')
---
+++ visualizationLDA.R
@@ -0,0 +1,49 @@
+###############################################################
+##### ###visualization of LDA model ##########################
+##############################################################
+
+#load results from LDA topic modelling
+load(Environment.RData)
+
+#instal for loading JSON file in explorer, if it doesnt work
+#install.packages('servr') 
+
+topicmodels_json_ldavis <- function(fitted, corpus, doc_term){
+  ## Required packages
+  library(topicmodels)
+  library(dplyr)
+  library(stringi)
+  library(tm)
+  library(LDAvis)
+  
+  fitted=DTM_model
+  corpus=corpusFirm
+  doc_term=gilead_DTM
+  
+  ## Find required quantities
+  phi=posterior(fitted)$terms %>% as.matrix
+  theta=posterior(fitted)$topics %>% as.matrix
+  vocab=colnames(phi)
+  doc_length <- vector()
+  for (i in 1:length(corpus$text)) {
+    temp <- paste(corpus[i]$text, collapse = ' ')
+    #vector of sums of words for each text in corpus
+    doc_length <- c(doc_length, stri_count(temp, regex = '\\S+'))
+  }
+  temp_frequency=slam::col_sums(doc_term) 
+  freq_matrix <- data.frame(ST = names(temp_frequency),
+                            Freq = temp_frequency)
+  rm(temp_frequency)
+  
+  ## Convert to json
+  json_lda <- LDAvis::createJSON(phi = phi, theta = theta,
+                                 vocab = vocab,
+                                 doc.length = doc_length,
+                                 term.frequency = freq_matrix$Freq)
+  
+  return(json_lda)
+}
+
+#supply fitted model, corpus and document term matrix
+gilead.json=topicmodels_json_ldavis(DTM_model, corpusFirm, gilead_DTM)
+serVis(gilead.json)
